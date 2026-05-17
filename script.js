1fe3b9cb3d35d0fe926babc6561553a1364be20d
lotchak79-spec <lotchak79@gmail.com>
2026-05-18 00:18:05 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-window.addEventListener('scroll', function() {
-    var image = document.querySelector('.features img.centered-image');
-    if (image) {
-        var scrolled = window.pageYOffset;
-        image.style.transform = 'translate3d(0, ' + (scrolled * 0.25) + 'px, 0)';
-    }
-});
+
